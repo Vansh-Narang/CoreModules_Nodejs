c57b381e007873fd663364ebbab0f7f4531de099
Vansh=Narang <vanshnarang49906@gmail.com>
2023-03-30 23:09:51 +0530

    ChallengeCOmpleted

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 const fs = require('fs');
 
-//read file
-//first read and then write
-// fs.writeFileSync("read.txt", "welcome to node js")
+// //read file
+// //first read and then write
+// // fs.writeFileSync("read.txt", "welcome to node js")
 
-//add data append
-// fs.appendFileSync('read.txt', "hello vansh")
-
-
-//read the data
-const ans = fs.readFileSync('read.txt');
-//while reading a file we always get buffer data
-console.log(ans);
+// //add data append
+// // fs.appendFileSync('read.txt', "hello vansh")
 
 
-org_data = ans.toString();
-//to string usd to convert buufer into org_data
-console.log(org_data);
+// //read the data
+// const ans = fs.readFileSync('read.txt');
+// //while reading a file we always get buffer data
+// console.log(ans);
 
 
-//to rename file
-fs.renameSync("read.txt", "vansh.txt")
+// org_data = ans.toString();
+// //to string usd to convert buufer into org_data
+// console.log(org_data);
+
+
+// //to rename file
+// fs.renameSync("read.txt", "vansh.txt")
 
 
 
-//used sync function to wait while one line is executed
-//as node js has asynchronous functions which doesnot wait while executing one line
+// //used sync function to wait while one line is executed
+// //as node js has asynchronous functions which doesnot wait while executing one line
+
+
+
+
+//Challenge
+fs.writeFileSync("Learn.txt", 'Some data')
+fs.appendFileSync("Learn.txt", "added more data")
+
+//read data
+const readdata = fs.readFileSync("Learn.txt", 'utf-8')
+console.log(readdata);
+
+//rename
+fs.renameSync("Learn.txt", "rename.txt")
+
+//delete
