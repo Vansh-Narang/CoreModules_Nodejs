e83b5f116d75ee3315f19b7bb2f51606f4924215
Vansh=Narang <vanshnarang49906@gmail.com>
2023-03-31 21:08:57 +0530

    Async done

--- aysnc.js
+++ aysnc.js
@@ -1,10 +1,34 @@
 const fs = require('fs')
-//learning asynchrnous functions
+const { useLayoutEffect } = require('react')
+// //learning asynchrnous functions
 
-//they must have callback with them
+// //they must have callback with them
 
-fs.readFile("vansh.txt", "utf-8", (err, data) => {
+// fs.readFile("vansh.txt", "utf-8", (err, data) => {
+//     console.log(data);
+// })
+// console.log("After the function");
+// //it is not necessary that the call made first is executed first in asynchornous functions
+
+
+//challenge
+//create a file name bio.txt
+//asynchronous
+fs.writeFile("Bio.txt", "my name is Vansh", (err) => {
+    console.log("file is created")
+})
+
+fs.appendFile("Bio.txt", "Hi Vansh", () => {
+})
+fs.readFile("Bio.txt", "utf-8", (err, data) => {
     console.log(data);
 })
-console.log("After the function");
-//it is not necessary that the call made first is executed first in asynchornous functions
+
+fs.rename("Bio.txt", "bio.txt", () => {
+    console.log("file renamed");
+})
+
+//delete unlink
+fs.unlink("bios.txt", () => {
+    console.log("file deleted");
+})
